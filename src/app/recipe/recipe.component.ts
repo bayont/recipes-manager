622cbdd5d2c9bc3ea26a3cd242a5699f9105845b
bayont <f.fetter02@gmail.com>
2023-01-16 19:00:01 +0100

    feat: introduce prettier

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -4,6 +4,6 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
   styleUrls: ['./recipe.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RecipeComponent {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  API_URL: 'https://crudcrud.com/api/2660cbe41c694a93b09b7eaf8103ec1f',
+  API_URL: 'https://crudcrud.com/api/2660cbe41c694a93b09b7eaf8103ec1f'
 };
