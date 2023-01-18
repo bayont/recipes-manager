d8ef2f6385b7a62685976da6fe28beca25bc3489
bayont <f.fetter02@gmail.com>
2023-01-18 16:16:58 +0100

    feat: add action buttons to recipe details component

--- src/app/recipe-details/recipe-details.component.ts
+++ src/app/recipe-details/recipe-details.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { combineLatest, Observable, of, Subject, switchMap, takeUntil } from 'rxjs';
 import { Recipe } from '../shared';
+import { actionDeleteRecipeWithDialog } from '../store/recipe.actions';
 import { RecipeListState } from '../store/recipe.reducer';
 import { selectRecipes } from '../store/recipe.select';
 
@@ -14,7 +15,11 @@ import { selectRecipes } from '../store/recipe.select';
 export class RecipeDetailsComponent implements OnInit, OnDestroy {
   private destructor: Subject<boolean> = new Subject();
 
-  constructor(private route: ActivatedRoute, private store: Store<RecipeListState>) {}
+  constructor(
+    private route: ActivatedRoute,
+    private store: Store<RecipeListState>,
+    private router: Router
+  ) {}
 
   public recipe$!: Observable<Recipe | undefined>;
 
@@ -27,8 +32,15 @@ export class RecipeDetailsComponent implements OnInit, OnDestroy {
       takeUntil(this.destructor),
       switchMap(([params, recipes]) => {
         const searchedRecipe = recipes.find((recipe) => params['id'] === recipe._id);
+        if (!searchedRecipe) {
+          this.router.navigateByUrl('/recipes');
+        }
         return of(searchedRecipe);
       })
     );
+  }
+
+  public deleteRecipe(recipe: Recipe): void {
+    this.store.dispatch(actionDeleteRecipeWithDialog({ recipe }));
   }
 }
