1cf0a2b98f9bdb87c4250b2f815ef136d0af9101
bayont <f.fetter02@gmail.com>
2023-01-18 17:27:31 +0100

    feat: add simple e2e tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,19 @@
-describe('My First Test', () => {
-  it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('app is running!')
-  })
-})
+describe('Recipes tests', () => {
+  it('should see homepage', () => {
+    cy.visit('/');
+    cy.contains('ngRecipes');
+  });
+  it('should add recipe', () => {
+    cy.visit('/recipes');
+    cy.contains('Create new recipe').click();
+    cy.get('#name').clear().type('Test recipe');
+    cy.get('#preparationTimeInMinutes').clear().type('35');
+    cy.get('#description').clear().type('Description of recipe created by automation testing');
+    cy.get('#save').click();
+  });
+  it('should find added recipe in searchbar', () => {
+    cy.visit('/recipes');
+    cy.get('#searchbar').type('Test recipe');
+    cy.get('#list .recipe-list-element').contains('Test recipe');
+  });
+});
