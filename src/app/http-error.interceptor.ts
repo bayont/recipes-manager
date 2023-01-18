ed42bdd5b82b3cfe75185c46633db76927460a7c
bayont <f.fetter02@gmail.com>
2023-01-18 16:14:04 +0100

    feat: add snackbar information about api key expiration

--- src/app/http-error.interceptor.ts
+++ src/app/http-error.interceptor.ts
@@ -7,13 +7,20 @@ import {
   HttpRequest,
   HttpResponse
 } from '@angular/common/http';
-import { catchError, Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { catchError, map, Observable, throwError } from 'rxjs';
 
+@Injectable()
 export class HttpErrorInterceptor implements HttpInterceptor {
+  constructor(private snackBar: MatSnackBar) {}
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
-        throw `[${error.status}] ${req.params} request to API failed`;
+        if (error.status === 0 || error.status === 400) {
+          this.snackBar.open('CrudCrud API key expired', 'CLOSE');
+        }
+        throw error;
       })
     );
   }
