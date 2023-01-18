24943dc3eabfe393da26d41d670348c68e6b1058
bayont <f.fetter02@gmail.com>
2023-01-18 16:21:26 +0100

    feat: add search feature for recipes

--- src/app/recipes-list/recipes-list.component.ts
+++ src/app/recipes-list/recipes-list.component.ts
@@ -1,7 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { Observable, of } from 'rxjs';
+import {
+  asyncScheduler,
+  combineLatest,
+  Observable,
+  of,
+  startWith,
+  switchMap,
+  throttleTime
+} from 'rxjs';
 import { RecipeHttpService } from '../services/recipe-http.service';
 import { Recipe } from '../shared';
 import {
@@ -19,12 +28,33 @@ import { selectRecipes } from '../store/recipe.select';
 })
 export class RecipesListComponent {
   public recipes$: Observable<Recipe[]> = of([]);
+  public searchBar = new FormControl('');
 
   constructor(private store: Store<RecipeListState>) {}
 
   ngOnInit(): void {
     this.store.dispatch(actionFetchRecipes());
-    this.recipes$ = this.store.select(selectRecipes);
+    this.recipes$ = combineLatest([
+      this.store.select(selectRecipes),
+      this.searchBar.valueChanges.pipe(startWith(''))
+    ]).pipe(
+      switchMap(([recipes, searchValue]) => {
+        if (!searchValue) {
+          return of(recipes);
+        }
+        return of(
+          recipes.filter((recipe) => {
+            const regex = new RegExp(searchValue, 'i');
+            for (const ingredient of recipe.ingredients) {
+              if (regex.test(ingredient.name)) {
+                return true;
+              }
+            }
+            return regex.test(recipe.name);
+          })
+        );
+      })
+    );
   }
 
   public addNewRecipe(): void {
